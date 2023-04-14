6b1a5a55f8cfd94e0cf3c7015f726c569680f3db
selcukgulll <57260606+selcukgulll@users.noreply.github.com>
2023-04-15 02:38:46 +0300

    Delete 3-Beginner Series #1 School Paperwork directory

--- 3-Beginner Series #1 School Paperwork/Beginner Series #1 School Paperwork.sql
+++
@@ -1,9 +0,0 @@
--- # write your SQL statement here: you are given a table 'paperwork' with columns 'id', n' and 'm', return a table with columns 'n', 'm' and your result in a column named 'res'.
-
-Select  n,m,
-  Case
-  WHEN n<0 OR m<0 THEN 0
-  ELSE n*m
-  End
-  as res
-  from paperwork;
